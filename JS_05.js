09ea871c1ada3b23b080de711c154bef10aa0470
Breentp <brenda.tiburcio.31@gmail.com>
2024-04-26 14:22:25 -0600

    JS_04.js

---
+++ JS_05.js
@@ -0,0 +1,38 @@
+//* User profile
+/**
+ * Escriba un programa que solicite al usuario sus datos: nombre de usuario, edad y una lista de sus películas favoritas. Almacene la información y luego muéstrela en la consola. Tenga en cuenta que el resultado de las películas debe agregar un pequeño mensaje como: 'La película {película} es una de mis favoritas'.
+ */
+
+
+
+//* Highest number
+/**
+ * Escribe un programa que solicite 10 números. Usando operadores lógicos y cualquier otra función/estructura de JavaScript que haya visto antes, determine y genere el mayor de esos números.
+ */
+
+
+
+//* Alarm
+/**
+ * Escribe un programa que pregunte a un usuario la cantidad de segundos necesarios hasta que se ejecute una alarma (mensaje) junto con un texto para mostrar una vez que hayan pasado esos segundos en tiempo real.
+ */
+
+
+
+//* Palindrome
+/**
+ * Escriba un programa que solicite una palabra u oración (puede estar en mayúscula, tener espacios y puntuación). Averigua si la oración/palabra es un palíndromo o no. Ignorando la puntuación, los espacios y las letras mayúsculas.
+ */
+
+
+//* Factorial
+/**
+ * Escriba un programa que solicite un número entero n. Donde 1 <= n. Resuelve esto usando recursividad.
+ */
+
+
+
+//* Flat array
+/**
+ * Escriba un programa que tome la siguiente matriz anidada y la aplane (convirtiéndola en una matriz 1D). Utilice cualquier tipo de algoritmo que desee, como devoluciones de llamada, recursividad, etc.
+ */
