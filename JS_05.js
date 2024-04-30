b68eaf6350e65182d4819e08c37add5f14d9e5c0
Breentp <brenda.tiburcio.31@gmail.com>
2024-04-30 17:39:51 -0600

    Se agregó la resolución de un ejercicio.

--- JS_05.js
+++ JS_05.js
@@ -2,7 +2,7 @@
 /**
  * Escriba un programa que solicite al usuario sus datos: nombre de usuario, edad y una lista de sus películas favoritas. Almacene la información y luego muéstrela en la consola. Tenga en cuenta que el resultado de las películas debe agregar un pequeño mensaje como: 'La película {película} es una de mis favoritas'.
  */
-let userNam = prompt ("Enter your user name: ");
+/**let userNam = prompt ("Enter your user name: ");
 let years = prompt ("Enter your age: ");
 let favMovies = [];
 
@@ -25,7 +25,7 @@ console.log(`${randomMov} is one of my favorite movies.`);
 /**
  * Escribe un programa que solicite 10 números. Usando operadores lógicos y cualquier otra función/estructura de JavaScript que haya visto antes, determine y genere el mayor de esos números.
  */
-
+/**
 const numbersList = [];
 
 for (i = 0; i <= 9; i++) {
@@ -42,7 +42,7 @@ console.log("The maximum number is: " + numberMax);
 /**
  * Escribe un programa que pregunte a un usuario la cantidad de segundos necesarios hasta que se ejecute una alarma (mensaje) junto con un texto para mostrar una vez que hayan pasado esos segundos en tiempo real.
  */
-
+/**
 var seconds = prompt ("Set the seconds to alarm: ");
 var miliSeconds = seconds * 1000;
 
@@ -58,6 +58,19 @@ setTimeout(() => {
  * Escriba un programa que solicite una palabra u oración (puede estar en mayúscula, tener espacios y puntuación). Averigua si la oración/palabra es un palíndromo o no. Ignorando la puntuación, los espacios y las letras mayúsculas.
  */
 
+let word = prompt ("Enter a word o phrase: ")
+
+function palindrome () {
+    const inverseWord = word.split("").reverse().join("");
+    if (word === inverseWord) {
+      console.log("This is a palindrome.");
+    } else {
+      console.log("This is not a palindrome.");
+    }
+  }
+  
+  console.log(palindrome(word));
+
 /////////////////////////////////////////////////////
 
 
@@ -65,7 +78,8 @@ setTimeout(() => {
 /**
  * Escriba un programa que solicite un número entero n. Donde 1 <= n. Resuelve esto usando recursividad.
  */
- let interNum = prompt ("Set a integer number: ");
+/** 
+let interNum = prompt ("Set a integer number: ");
 
  function factorial(n) {
     if (n > 1) {
@@ -73,7 +87,7 @@ setTimeout(() => {
     } else if (n === 1) {
         return 1;
     } else {
-        console.log("Solo se calcula el factorial de número positivos > 0")
+        console.log("Only enter positive numbers.")
     }
  }
  console.log(factorial(interNum));
@@ -83,5 +97,5 @@ setTimeout(() => {
 /**
  * Escriba un programa que tome la siguiente matriz anidada y la aplane (convirtiéndola en una matriz 1D). Utilice cualquier tipo de algoritmo que desee, como devoluciones de llamada, recursividad, etc.
  */
-
-let multiDimension = [1, [2, [3, [4, 5, [6]]]]];
+/**
+let multiDimension = [1, [2, [3, [4, 5, [6]]]]]; */
